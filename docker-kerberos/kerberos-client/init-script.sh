586b42540180432aa01aedecdaf2d2f055617ae6
Simão Martins <simao.martins@tecnico.ulisboa.pt>
2016-05-05 17:10:15 +0100

    Send coverage report.

--- docker-kerberos/kerberos-client/init-script.sh
+++ docker-kerberos/kerberos-client/init-script.sh
@@ -3,11 +3,10 @@
 source `dirname $0`/configureKerberosClient.sh
 
 cd /tmp/akka-kadmin
-sbt test
-#sbt coverage test
-#sbt coverageReport
-#sbt coverageAggregate
-#sbt codacyCoverage
+sbt coverage test
+sbt coverageReport
+sbt coverageAggregate
+sbt codacyCoverage
 
 #echo "RunOnly"
 #sbt <<<"testOnly pt.tecnico.dsi.kadmin.PrincipalSpec"
